4327b5f1209ef0c3ce5b53334315c915f803588a
hayk98-hayrapetyan <hayk.hayrapetyan98@gmail.com>
2022-09-29 00:54:08 +0400

    setup project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-</template>
+<script setup lang="ts">
+import BaseButton from "@/components/atoms/base-button/BaseButton.vue";
+import FilterBoard from "@/components/organisms/filter-board/FilterBoard.vue";
+import FilterModal from "./components/organisms/filter-modal/FilterModal.vue";
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { reactive, ref } from "vue";
+import { countries } from "@/assets/data/coutries";
+import { regions } from "@/assets/data/regions";
+import { COMPONENT } from "@/enums/filter";
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+const generateUniqueId = () =>
+  new Date().getMilliseconds() + Math.floor(Math.random() * 100);
+
+const filters = reactive([
+  {
+    title: "Location",
+    filters: [
+      {
+        title: "Country",
+        filters: [
+          {
+            searchable: true,
+            component: COMPONENT.CHECKBOX,
+            id: generateUniqueId(),
+            value: countries.map((country) => ({
+              value: country,
+              label: country,
+              modelValue: false,
+            })),
+          },
+        ],
+      },
+      {
+        title: "Regions",
+        filters: [
+          {
+            searchable: true,
+            component: COMPONENT.CHECKBOX,
+            id: generateUniqueId(),
+            value: regions.map((region) => ({
+              value: region,
+              label: region,
+              modelValue: false,
+            })),
+          },
+        ],
+      },
+    ],
   },
-});
+]);
+
+const renderModal = ref(false);
+
+const toggleFilters = () => {
+  renderModal.value = !renderModal.value;
+};
 </script>
 
+<template>
+  <div>
+    <div class="app__filters">
+      <FilterBoard
+        v-for="filter in filters"
+        :key="filter.title"
+        v-bind="filter"
+      />
+      <BaseButton @click="toggleFilters" class="px-15 ml-10">
+        Advanced filters
+      </BaseButton>
+    </div>
+
+    <FilterModal v-if="renderModal" @close="toggleFilters" />
+  </div>
+</template>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app__filters {
+  padding: 40px;
+  display: flex;
 }
 </style>
